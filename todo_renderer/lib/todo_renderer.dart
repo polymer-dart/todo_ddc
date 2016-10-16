fee21be94dd50ffc3aa06bbece5bae8f98e30e99
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-10-16 14:38:27 +0200

    going to support wrapped elements

--- todo_renderer/lib/todo_renderer.dart
+++ todo_renderer/lib/todo_renderer.dart
@@ -1,12 +1,19 @@
 import 'dart:html';
 import 'package:todo_common/model.dart';
 import 'package:polymer_element/polymer_element.dart';
+import 'package:polymer_element/paper_dialog.dart';
 import 'dart:async';
 
 @PolymerRegister('todo-renderer', template: 'todo_renderer.html')
 class TodoRenderer extends PolymerElement {
   TodoDTO myTodo;
   bool editing = false;
+  bool askConfirm = false;
+
+  @Observe('askConfirm')
+  void debugOpened(_) {
+    print("Dialog open state has changed!");
+  }
 
   startEdit(Event ev, details) {
     editing = true;
@@ -29,6 +36,13 @@ class TodoRenderer extends PolymerElement {
   }
 
   removeMe(Event ev,details) {
+    //askConfirm = true;
+    PaperDialog dlg = shadowRoot.querySelector('#dlg');
+    dlg.open();
+    //print("DIALOG : ${dlg} , ${dlg.opened}");
+  }
+
+  removeConfirmed(Event ev,details) {
     dispatchEvent(createCustomEvent('remove-me',myTodo));
   }
 }
